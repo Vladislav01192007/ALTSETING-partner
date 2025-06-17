6638d7daaf7f4835e07d3107a92bbc446c7eeb45
ALTSETING <vladislav.melnyk.kripto@gmail.com>
2025-06-17 18:48:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector("form");
 
-  form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Дякуємо! Ваше повідомлення надіслано.");
-    form.reset();
-  });
-});
